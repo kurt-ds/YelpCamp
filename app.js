beeece8fd5c1806310277a70f65771f1301a56ef
Kurt D-S <delos.santos.kurt.allen@gmail.com>
2023-07-18 15:54:06 +0800

    removed comments

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 
 const MongoStore = require('connect-mongo');
 
-//Local Server Url "mongodb://127.0.0.1:27017/yelp-camp"
+
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
